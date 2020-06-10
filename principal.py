b262a13de230b18b15b17e65672dcfd76fc4dc1f
ezequiel.lamarque <ezequiel.lamarque@globant.com>
2020-06-10 11:44:43 -0300

    include cropCascade to principal script

--- principal.py
+++ principal.py
@@ -5,6 +5,7 @@ import numpy as np
 from utils.modelNN.model import load
 from utils.faceSpotDetection.faceSpotsDetector import generateAllColoursMask
 from utils.eyesDetection.colorEyeDetector import getPrincipalEyeColourFromFrame
+from utils.cropCascade import cropCascade
 
 # constants
 PATH_MODAL = './model/prueba100fotos_inv_2.sh'
@@ -16,11 +17,17 @@ model = load(os.path.abspath(PATH_MODAL))
 # Face cascade to detect faces
 face_cascade = cv2.CascadeClassifier(os.path.abspath(PATH_CASCADE))
 
-frame = cv2.imread('./media/input/spots/negro/cat1.jpg',cv2.IMREAD_COLOR)
+# get the image from the params & show the original
+imageName = sys.argv[1]
+frame = cv2.imread('./media/input/test/'+imageName+'.jpg',cv2.IMREAD_COLOR)
 cv2.imshow('originalFrame', frame)
 cv2.waitKey(0)
-maskFrame = generateAllColoursMask(frame)
-getPrincipalEyeColourFromFrame(frame, model, face_cascade)
+
+# crop the cat face (if there is a cat face) & get the mask
+cropedFrame = cropCascade(frame,face_cascade)
+maskFrame = generateAllColoursMask(cropedFrame)
 cv2.imshow('faceMask', maskFrame)
+# get the principal colour of the eyes cat
+getPrincipalEyeColourFromFrame(cropedFrame, model, face_cascade)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
